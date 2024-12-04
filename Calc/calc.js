d2f23c7d5404b781c43ceb34d060da362b8eefea
12862 <daniel.gosses@vtsd.com>
2024-12-04 08:41:04 -0500

    24-25 Java Script

---
+++ Calc/calc.js
@@ -0,0 +1,29 @@
+const buttons = document.querySelectorAll("button")
+const display = document.querySelector('.display');
+
+// Add listener to all buttons
+buttons.forEach(
+    function(button){
+     button.addEventListener("click",calculate);
+    }
+);
+
+function calculate(event){
+    //What button was clicked?
+    const clickedValue = event.target.value;
+
+    if(clickedValue === "="){
+        // Is the display empty?
+        if(display.value !== ""){
+            //Calculate the value and display it
+            display.value = eval(display.value);
+        }
+    } else if(clickedValue === "C"){
+    }else{
+        display.value += clickedValue;
+    }
+
+}
+
+
+
